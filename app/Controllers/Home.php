1fbe885e7d29c65ddfd46e2e793a7e407158d940
violetachaitra <violetacorvala@gmail.com>
2025-06-13 11:41:19 +0700

    first commit

---
+++ app/Controllers/Home.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\ProductModel; 
+
+class Home extends BaseController
+{
+    protected $product;
+    function __construct()
+    {
+        helper('form');
+        helper('number');
+        $this->product = new ProductModel();
+    }
+    public function index(): string
+    {
+        $product = $this->product->findAll();
+        $data['product'] = $product;
+
+        return view('v_home', $data);
+    }
+}
